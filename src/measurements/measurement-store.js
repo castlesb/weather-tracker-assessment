cbcd28535a79afccbb8244b5908479d2bba8e78b
HackerRank <hackerrank@example.com>
2019-01-21 20:18:15 +0000

    Saved at Mon Jan 21 2019 15:18:15 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -17,7 +17,7 @@ export function add(measurement) {
  * @returns {Measurement} measurement for the particular date
  */
 export function fetch(timestamp) {
-  throw new HttpError(501);
+  
 }
 
 /**
