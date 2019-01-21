fdbf96b10393d2c2e51fc99195fb0e6ab078cbb1
HackerRank <hackerrank@example.com>
2019-01-21 21:18:57 +0000

    Saved at Mon Jan 21 2019 16:18:57 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -26,5 +26,5 @@ export function fetch(timestamp) {
  * @param {Date} end Upper bound for the query, exclusive
  */
 export function queryDateRange(from, to) {
-  cstore.findIndex(e => e.timestamp.toISOString === from.toISOString());
+  const ststore.findIndex(e => e.timestamp.toISOString === from.toISOString());
 }
