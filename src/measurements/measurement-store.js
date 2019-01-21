ad94fd5decec9d2ee3300a5c29354f2a7239155c
HackerRank <hackerrank@example.com>
2019-01-21 20:49:13 +0000

    Saved at Mon Jan 21 2019 15:49:12 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -17,6 +17,7 @@ export function add(measurement) {
  * @returns {Measurement} measurement for the particular date
  */
 export function fetch(timestamp) {
+
   return store.find(element => element.timestamp === timestamp);
 }
 
