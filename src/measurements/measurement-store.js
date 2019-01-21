2d92e96f742144cc5127746c6a370290b2160016
HackerRank <hackerrank@example.com>
2019-01-21 21:41:36 +0000

    Saved at Mon Jan 21 2019 16:41:36 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -31,7 +31,7 @@ export function queryDateRange(from, to) {
 
   for (let i = start; i < store.length; i++) {
     if (store[i].timestamp.toISOString() === to.toISOString()) {
-        
+        break;
     } else {
         result.push(store[i]);
     }
