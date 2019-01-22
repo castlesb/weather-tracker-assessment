2f771d92d6fc941c96d066cb13384051480ae688
HackerRank <hackerrank@example.com>
2019-01-22 00:10:45 +0000

    Saved at Mon Jan 21 2019 19:10:45 GMT-0500 (Eastern Standard Time)

--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -9,7 +9,7 @@ import { Measurement } from '../measurements/measurement';
  * @return {*}
  */
 export function computeStats(measurements, metrics, stats) {
-  let min = ;
+  let min = m;
   let max = 0;
   let avg = 0;
 
