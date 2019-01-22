4d4f8fac4071e861fc33d0e599d6bb0164274358
HackerRank <hackerrank@example.com>
2019-01-22 21:00:44 +0000

    Saved at Tue Jan 22 2019 16:00:43 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -48,7 +48,7 @@ function binarySearch(timestamp) {
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
 
-    if (arr[mid].timestamp. === timestamp) return true;
+    if (arr[mid].timestamp.t === timestamp) return true;
     else if (arr[mid] < x)
       start = mid + 1;
     else
