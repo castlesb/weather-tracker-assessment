70ae8802f53b269369913b661115d59c0a1361da
HackerRank <hackerrank@example.com>
2019-01-22 01:33:23 +0000

    Saved at Mon Jan 21 2019 20:33:23 GMT-0500 (Eastern Standard Time)

--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -11,11 +11,10 @@ import { Measurement } from '../measurements/measurement';
 export function computeStats(measurements, metrics, stats) {
   const result = [];
   for (let metric of metrics) {
-      const metricArray = measurements.map(m => m[metric]);
-      console.log(`METRIC ARRAY: ${JSON.stringify(metricArray)}`);
+
     for (let stat of stats) {
       if (stat === 'min') {
-        
+        result.push({metric, stat: 'min', value: arrayMin(metricArray)});
       } else if (stat === 'max') {
         const max = Math.max(...metricArray);
         result.push({metric, stat: 'max', value: max});
