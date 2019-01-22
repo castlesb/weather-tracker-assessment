5dd35f29685405a225a07678adbf11474c921b1c
HackerRank <hackerrank@example.com>
2019-01-22 22:56:32 +0000

    Saved at Tue Jan 22 2019 17:56:32 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -58,3 +58,19 @@ function binarySearch(timestamp) {
   }
 }
 
+function binarySearch(timestamp) {
+  let start = 0;
+  let end = store.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (store[mid].timestamp.getTime() === timestamp.getTime()) {
+      return store[mid];
+    } else if (store[mid].timestamp.getTime() < timestamp.getTime()) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+}
