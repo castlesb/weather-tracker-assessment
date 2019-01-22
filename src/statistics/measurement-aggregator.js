f9f7750f0876e5926a846ae2f3a76da7c463ff7a
HackerRank <hackerrank@example.com>
2019-01-22 00:10:09 +0000

    Saved at Mon Jan 21 2019 19:10:09 GMT-0500 (Eastern Standard Time)

--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -14,7 +14,7 @@ export function computeStats(measurements, metrics, stats) {
   let avg = 0;
 
   for (let measurement of measurements) {
-    for (let metric of metr)
+    for (let metric of metrics)
   }
 /*
   for (let stat of stats) {
