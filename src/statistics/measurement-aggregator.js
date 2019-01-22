156de42d34a5e4bc22cb49c43ba6302b21c59004
HackerRank <hackerrank@example.com>
2019-01-22 00:30:55 +0000

    Saved at Mon Jan 21 2019 19:30:55 GMT-0500 (Eastern Standard Time)

--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -14,7 +14,7 @@ export function computeStats(measurements, metrics, stats) {
     for (let stat of stats) {
       if (stat === 'min') {
         const min = Math.min(...measurements.metric);
-        result.push({metric, stat: 'min', vmin});
+        result.push({metric, stat: 'min', value: min});
       }
     }
   }
