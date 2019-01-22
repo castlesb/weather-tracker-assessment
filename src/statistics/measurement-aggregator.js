2cbaeb5a6e330369843867e67b960b26a3479498
HackerRank <hackerrank@example.com>
2019-01-22 01:31:39 +0000

    Saved at Mon Jan 21 2019 20:31:39 GMT-0500 (Eastern Standard Time)

--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -32,7 +32,7 @@ export function computeStats(measurements, metrics, stats) {
 }
 
 function arrayMin(arr) {
-  return arr.reduce((a, b) => {
+  return arr.reduce((a, b) {
     return (a < b ? a : b);
   });
 }
