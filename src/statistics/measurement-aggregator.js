342598066d4b4292edf4e5c6b07e3e57b91e4cd4
HackerRank <hackerrank@example.com>
2019-01-22 18:03:10 +0000

    Saved at Tue Jan 22 2019 13:03:09 GMT-0500 (Eastern Standard Time)

--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -11,7 +11,7 @@ import { Measurement } from '../measurements/measurement';
 export function computeStats(measurements, metrics, stats) {
   const result = [];
   console.log(`MEASUREMENTS ARRAY: ${JSON.stringify(measurements)}`);
-  console.log(`MEASUREMENTS[0] data: ${JSON.stringify(measurements[0].tempura)}`);
+  console.log(`MEASUREMENTS[0] data: ${JSON.stringify(measurements[0].tempurature)}`);
   for (let metric of metrics) {
       const metricArray = measurements.map(m => m[metric]);
       console.log(`METRIC ARRAY: ${JSON.stringify(metricArray)}`);
