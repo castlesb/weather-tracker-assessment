b4ef4634c11b2847b32dbb38d8ce7ad81886e5d5
HackerRank <hackerrank@example.com>
2019-01-22 02:00:57 +0000

    Saved at Mon Jan 21 2019 21:00:57 GMT-0500 (Eastern Standard Time)

--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -9,7 +9,7 @@ import { Measurement } from '../measurements/measurement';
  * @return {*}
  */
  
-
+/*
 export function computeStats(measurements, metrics, stats) {
   const result = [];
   console.log(`MEASUREMENTS ARRAY: ${JSON.stringify(measurements)}`);
