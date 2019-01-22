315c9f7525fc2dff859d39603ae52e7efd2c0803
HackerRank <hackerrank@example.com>
2019-01-22 20:56:00 +0000

    Saved at Tue Jan 22 2019 15:55:59 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -45,5 +45,20 @@ function binarySearch(timestamp) {
   let start = 0;
   let end = arr.length-1;
   
+  while (start<=end){ 
   
+        // Find the mid index 
+        let mid=Math.floor((start + end)/2); 
+   
+        // If element is present at mid, return True 
+        if (arr[mid]===x) return true; 
+  
+        // Else look in left or right half accordingly 
+        else if (arr[mid] < x)  
+             start = mid + 1; 
+        else
+             end = mid - 1; 
+    } 
+   
+    return false;
 }
