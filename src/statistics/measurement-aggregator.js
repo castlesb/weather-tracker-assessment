c6d1dffaa235d6cf8429f4e22d1e7d580f5b93a9
HackerRank <hackerrank@example.com>
2019-01-22 01:36:22 +0000

    Saved at Mon Jan 21 2019 20:36:22 GMT-0500 (Eastern Standard Time)

--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -12,7 +12,7 @@ export function computeStats(measurements, metrics, stats) {
   const result = [];
   for (let metric of metrics) {
       const metricArray = measurements.map(m => m[metric]);
-      console.log(`METRIC ARRAY: ${JSON.stringify(measurements)}`);
+      
       console.log(`METRIC ARRAY: ${JSON.stringify(measurements)}`);
     for (let stat of stats) {
       if (stat === 'min') {
