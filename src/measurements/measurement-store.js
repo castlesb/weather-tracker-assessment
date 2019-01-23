53d10f9034c1e54b50b7d24bfd30bbbef1c86009
HackerRank <hackerrank@example.com>
2019-01-23 18:55:28 +0000

    Saved at Wed Jan 23 2019 13:55:28 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -10,7 +10,7 @@ const store = [];
  */
 export function add(measurement) {
   measurement.timestamp = measurement.timestamp.getTime();
-  console.log('timestamp:' + measurement.timestamp);
+  console.log('timestamp:')
   store.push(measurement);
 }
 
