b2e1e25b4e0addddcd4ba2f8a716a222de55ff90
HackerRank <hackerrank@example.com>
2019-01-23 18:45:53 +0000

    Saved at Wed Jan 23 2019 13:45:53 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -9,7 +9,7 @@ const store = [];
  * @param {Measurement} measurement to be added
  */
 export function add(measurement) {
-  measurement.timestamp = measurement.timestamp.get
+  measurement.timestamp = measurement.timestamp.getTime()
   store.push(measurement);
 }
 
