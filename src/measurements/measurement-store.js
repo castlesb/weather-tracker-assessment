66f02906d620f34b8213678bb6169fc701f60799
HackerRank <hackerrank@example.com>
2019-01-23 18:36:55 +0000

    Saved at Wed Jan 23 2019 13:36:55 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -21,7 +21,7 @@ export function fetch(timestamp) {
   console.log('TIME: ' + timestamp.getTime());
   // return store.find(e => e.timestamp.toISOString() === timestamp.toISOString());
   const index = binarySearch2(timestamp);
-  
+  r
 }
 
 /**
