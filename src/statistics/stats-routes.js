f434d081d65c28b8b7dd1e614090ce332936f94c
HackerRank <hackerrank@example.com>
2019-01-24 02:21:18 +0000

    Saved at Wed Jan 23 2019 21:21:18 GMT-0500 (Eastern Standard Time)

--- src/statistics/stats-routes.js
+++ src/statistics/stats-routes.js
@@ -26,7 +26,7 @@ function asArray(val) {
   return Array.isArray(val) ? val : [val];
 }
 
-function parseInpu({ timestamp, ...metrics }) {
+function parseI({ timestamp, ...metrics }) {
   const measurement = new Measurement();
   measurement.timestamp = new Date(timestamp);
 
