de65600c0b5cc1931f8025cf16c1ca0740451151
HackerRank <hackerrank@example.com>
2019-01-24 16:10:25 +0000

    Saved at Thu Jan 24 2019 11:10:25 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -33,7 +33,7 @@ export function queryDateRange(from, to) {
 
   // const start = store.findIndex(e => e.timestamp.toISOString() === from.toISOString());
   const start = binarySearch(from);
-  let i = start;
+  /let i = start;
   
   for ()
   
