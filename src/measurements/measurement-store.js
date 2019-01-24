8cf6affea706e1dc7d719d6b1f316ab494569348
HackerRank <hackerrank@example.com>
2019-01-24 16:26:52 +0000

    Saved at Thu Jan 24 2019 11:26:52 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -44,7 +44,7 @@ export function queryDateRange(from, to) {
   return result;
 }
 
-// Search for 
+// Search for the 
 function binarySearch(timestamp) {
   let start = 0;
   let end = store.length - 1;
