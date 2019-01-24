f10d358d1d150f9f84bd26b994c3cc62cc97f7d1
HackerRank <hackerrank@example.com>
2019-01-24 02:20:57 +0000

    Saved at Wed Jan 23 2019 21:20:57 GMT-0500 (Eastern Standard Time)

--- src/statistics/stats-routes.js
+++ src/statistics/stats-routes.js
@@ -26,3 +26,20 @@ function asArray(val) {
   return Array.isArray(val) ? val : [val];
 }
 
+function parseMeasurement({ timestamp, ...metrics }) {
+  const measurement = new Measurement();
+  measurement.timestamp = new Date(timestamp);
+
+  if (isNaN(measurement.timestamp)) throw new HttpError(400);
+
+  for (const metric in metrics) {
+    if (!metrics.hasOwnProperty(metric)) continue;
+
+    const value = metrics[metric];
+    if (isNaN(value)) throw new HttpError(400);
+
+    measurement.setMetric(metric, +value);
+  }
+
+  return measurement;
+}
