fea1eee51e395b9d7543594251ce2ff986eba06e
HackerRank <hackerrank@example.com>
2019-01-24 16:16:54 +0000

    Saved at Thu Jan 24 2019 11:16:54 GMT-0500 (Eastern Standard Time)

--- src/statistics/stats-routes.js
+++ src/statistics/stats-routes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
   const fromDateTime = new Date(req.query.fromDateTime);
   const toDateTime = new Date(req.query.toDateTime);
   
-  if (fro)
+  if (fromDateTime.get)
 
   const measurements = queryDateRange(fromDateTime, toDateTime);
 
