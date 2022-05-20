900601160ac47aea1e05f26caece764d37fead27
Brian Castles <brian.castles@tensilepayments.com>
2022-05-20 13:24:15 -0400

    Initial setup

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -9,7 +9,7 @@ const store = [];
  * @param {Measurement} measurement to be added
  */
 export function add(measurement) {
-  store.push(measurement);
+  // implement
 }
 
 /**
@@ -18,9 +18,7 @@ export function add(measurement) {
  * @returns {Measurement} measurement for the particular date
  */
 export function fetch(timestamp) {
-  // return store.find(e => e.timestamp.getTime() === timestamp.getTime());
-  const index = binarySearch(timestamp);
-  return store[index];
+  // implement
 }
 
 /**
@@ -29,36 +27,5 @@ export function fetch(timestamp) {
  * @param {Date} end Upper bound for the query, exclusive
  */
 export function queryDateRange(from, to) {
-  const result = [];
-
-  if (from.getTime() > to.getTime()) throw new HttpError(400);
-
-  // const start = store.findIndex(e => e.timestamp.getTime() === from.getTime());
-  const start = binarySearch(from);
-
-  for (let i = start; i < store.length; i++) {
-    if (store[i].timestamp.getTime() === to.getTime()) break;
-    result.push(store[i]);
-  }
-
-  return result;
-}
-
-// Search for the timestamp in the store using binary search and returns the index
-// Better performance for a store with hundreds of entries
-function binarySearch(timestamp) {
-  let start = 0;
-  let end = store.length - 1;
-
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-
-    if (store[mid].timestamp.getTime() === timestamp.getTime()) {
-      return mid;
-    } else if (store[mid].timestamp.getTime() < timestamp.getTime()) {
-      start = mid + 1;
-    } else {
-      end = mid - 1;
-    }
-  }
+  // Implement
 }
--- src/statistics/measurement-aggregator.js
+++ src/statistics/measurement-aggregator.js
@@ -9,24 +9,5 @@ import { Measurement } from '../measurements/measurement';
  * @return {*}
  */
 export function computeStats(measurements, metrics, stats) {
-  const result = [];
-
-  for (let metric of metrics) {
-      const metricArray = measurements.filter(m => m.getMetric(metric) != null).map(m => m.getMetric(metric));
-      if (metricArray.length === 0) return result;
-    for (let stat of stats) {
-      if (stat === 'min') {
-        const min = metricArray.reduce((a, b) => (a < b ? a : b));
-        result.push({metric, stat: 'min', value: min});
-      } else if (stat === 'max') {
-        const max = metricArray.reduce((a, b) => (a > b ? a : b));
-        result.push({metric, stat: 'max', value: max});
-      } else {
-        const average = metricArray.reduce((a, b) => a + b) / metricArray.length;
-        result.push({metric, stat: 'average', value: Math.round(average * 10) / 10});
-      }
-    }
-  }
-
-  return result;
+  // implement
 }
